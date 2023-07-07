35d2e58d2b81caf3b88cf4759e028d6d572fa800
Suraneti Rodsuwan <suraneti.rod@gmail.com>
2023-07-07 14:00:43 +0700

    chore: update ci.yaml

--- generate_markdown.js
+++ generate_markdown.js
@@ -1,40 +1,33 @@
 const fs = require('fs');
 
-function getCoverageStatus(coverage) {
-  const coveredPercentage = (coverage.lines.covered / coverage.lines.total) * 100;
+function jsonToMarkdown(jsonData) {
+  let markdown = '| File                               | Lines Covered | Total Lines | Functions Covered | Total Functions | Branches Covered | Total Branches |\n';
+  markdown += '|------------------------------------|---------------|-------------|-------------------|-----------------|------------------|----------------|\n';
 
-  if (coveredPercentage >= 80) {
-    return '![coverage](https://img.shields.io/badge/coverage-100%25-brightgreen)';
-  } else if (coveredPercentage >= 60) {
-    return '![coverage](https://img.shields.io/badge/coverage-80%25-yellow)';
-  } else {
-    return '![coverage](https://img.shields.io/badge/coverage-60%25-red)';
-  }
-}
+  for (const filePath in jsonData) {
+    const coverageData = jsonData[filePath];
+    const linesCovered = Object.values(coverageData.l).reduce((acc, val) => acc + val, 0);
+    const totalLines = Object.keys(coverageData.l).length + linesCovered;
+    const functionsCovered = Object.keys(coverageData.fnMap).length;
+    const totalFunctions = Object.keys(coverageData.f).length;
+    const branchesCovered = Object.keys(coverageData.branchMap).length;
+    const totalBranches = Object.keys(coverageData.b).length;
 
-function generateMarkdown(coverageData) {
-  let markdown = `| File                               | Lines Covered | Total Lines | Functions Covered | Total Functions | Branches Covered | Total Branches |
-|------------------------------------|---------------|-------------|-------------------|-----------------|------------------|----------------|\n`;
-
-  for (const [filePath, coverage] of Object.entries(coverageData)) {
-    const coverageStatus = getCoverageStatus(coverage);
-    const lineCoverage = `${coverage.lines.covered} / ${coverage.lines.total}`;
-    const functionCoverage = `${coverage.functions.covered} / ${coverage.functions.total}`;
-    const branchCoverage = `${coverage.branches.covered} / ${coverage.branches.total}`;
-
-    markdown += `| ${filePath.padEnd(35)} | ${lineCoverage.padEnd(14)} | ${functionCoverage.padEnd(12)} | ${branchCoverage.padEnd(17)} |\n`;
+    markdown += `| ${filePath} | ${linesCovered} | ${totalLines} | ${functionsCovered} | ${totalFunctions} | ${branchesCovered} | ${totalBranches} |\n`;
   }
 
   return markdown;
 }
 
-fs.readFile('./coverage.json', 'utf8', (err, data) => {
+// Example usage
+const jsonFilePath = './coverage.json';
+fs.readFile(jsonFilePath, 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
+    console.error(`Error reading JSON file: ${err}`);
     return;
   }
 
-  const coverageData = JSON.parse(data);
-  const markdown = generateMarkdown(coverageData);
+  const jsonData = JSON.parse(data);
+  const markdown = jsonToMarkdown(jsonData);
   console.log(markdown);
 });
